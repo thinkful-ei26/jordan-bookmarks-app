70b0b0615845fe18c6bf49518d4c0a1acfc647dd
Jordan <jordanhaddadi@gmail.com>
2018-11-08 11:59:31 -0500

    first message

---
+++ api.js
@@ -0,0 +1,15 @@
+store: {
+
+    bookmarks:
+    [{name: "ny times",
+    id: 1,
+    description: "lorem ipsem",
+    rating: 5,
+    expanded: false},
+    
+    {name: "the onion",
+    id: 2,
+    description: "lorem ipsem",
+    expanded: false}]
+
+}
